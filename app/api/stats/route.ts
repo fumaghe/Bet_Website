a3986160f3b06412f0ded6f2cf6c4fda32bbe2c8
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2025-01-26 15:12:59 +0100

    S

--- app/api/stats/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { loadStats, getAllStats } from '@/lib/services/stats-service';
-
-export async function GET() {
-  await loadStats();
-  const stats = getAllStats();
-  return NextResponse.json(stats);
-}
--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }, // Puoi mantenere questa opzione
+  images: { 
+    unoptimized: true 
+  },
+  basePath: '/Bet_Website', // Sostituisci con il nome del tuo repository
+  assetPrefix: '/Bet_Website/', // Sostituisci con il nome del tuo repository
+  output: 'export', // Necessario per l'esportazione statica
+  trailingSlash: true, // Opzionale: aggiunge una barra finale a ogni URL
 };
 
 module.exports = nextConfig;
